ab89081ea1ad410493683bc14c097adbab500355
Tayfun Yilmaz <tayfilmaz@gmail.com>
2019-07-24 11:00:07 +0200

    Added Readme.md

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# The Simple Ionic Powered Calculator
+Simple calculator using Ionic and Math.js.
+
+If you like my project, "Star" in the corresponding project right corner, please. If you want to buy me a cup of coffee, use the Bitcoin QR Code right below.
+
+<div align="center">
+  <img src="btcdonatewallet.jpg" />
+</div>
+
+BTC Address: 14pmyZZrFfCCEK49FC7jKWj1WGzhUP4rsQ
+
+
+## Installation
+
+Install dependencies.
+
+```js
+git clone https://github.com/localhostport80/simplecalc && cd simplecalc
+npm install
+```
+
+## Usage
+
+### DevApp
+
+Run it inside Ionic DevApp.
+
+```bash
+ionic serve --devapp
+```
+
+### Build
+
+Build it for the specific platform.
+
+```bash
+ionic cordova build --release <platform>
+```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Events} from '@ionic/angular';
+
+import {
+    evaluate
+} from 'mathjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    inputField = '';
+    lastBracket = '';
+    lastDeletedCharacter = '';
+
+    constructor() {
+    }
+
+    onBracketButtonClick() {
+        if (this.lastBracket === '' || this.lastBracket === ')') {
+            this.onDefaultButtonClick('(');
+            this.lastBracket = '(';
+        } else {
+            this.onDefaultButtonClick(')');
+            this.lastBracket = ')';
+        }
+    }
+
+    onDefaultButtonClick(event) {
+        this.inputField += event;
+    }
+
+    onResultButtonClick() {
+        this.inputField = evaluate(this.inputField);
+    }
+
+    swipeDownEvent() {
+        this.onResultButtonClick();
+    }
+
+    onClearButtonClick() {
+        this.inputField = '';
+    }
+
+    swipeUpEvent() {
+        this.onClearButtonClick();
+    }
+
+    onInputClick() {
+        this.inputField = this.inputField.substring(0, this.inputField.length - 1);
+    }
+
+    swipeLeftEvent() {
+        this.lastDeletedCharacter = this.inputField[this.inputField.length - 1];
+        this.inputField = this.inputField.substring(0, this.inputField.length - 1);
+    }
+
+    swipeRightEvent() {
+        this.inputField += this.lastDeletedCharacter;
+    }
 
 }
